c4c8c76bff1b30fc4e718ec3e74a1e9a3fbffa6a
Viresh Naik <110287118+Vireshgit@users.noreply.github.com>
2023-07-25 09:17:44 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,7 @@
 def call(string userName, string imageName, string imageTag){
-sh '''docker build -t ${userName}/${imageName} .
-docker tag ${userName}/${imageName} ${userName}/${imageName}:${imageTag}
-docker tag ${userName}/${imageName} ${userName}/${imageName}:latest'''
+    sh '''
+    docker build -t ${userName}/${imageName} .
+    docker tag ${userName}/${imageName} ${userName}/${imageName}:${imageTag}
+    docker tag ${userName}/${imageName} ${userName}/${imageName}:latest
+    '''
 }
