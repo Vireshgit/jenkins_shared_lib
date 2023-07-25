7e3c47ba7d587149f00296c506bb7fd82c3e02e6
Viresh Naik <110287118+Vireshgit@users.noreply.github.com>
2023-07-25 08:54:01 +0530

    Create dockerBuild.groovy

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,5 @@
+def call(string userName, string imageName, string imageTag){
+sh '''docker build -t ${userName}/${imageName} .
+docker tag ${userName}/${imageName} ${userName}/${imageName}:${imageTag}
+docker tag ${userName}/${imageName} ${userName}/${imageName}:latest'''
+}
