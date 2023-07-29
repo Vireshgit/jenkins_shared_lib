225cf53e749f918b187e6586b298f31ebbf63111
Viresh Naik <110287118+Vireshgit@users.noreply.github.com>
2023-07-29 12:07:10 +0530

    Update pushImagedhub.groovy

--- vars/pushImagedhub.groovy
+++ vars/pushImagedhub.groovy
@@ -4,7 +4,7 @@ def call(String hubUserName, String imageName, String imageTag){
               passwordVariable: 'PASS', 
               usernameVariable: 'USER'
     )]) {
-      sh "docker login -u $USER -p $PASS"
+      sh 'docker login -u $USER -p $PASS'
     }
     sh '''    docker push -t ${hubUserName}/${imageName}:${imageTag}
     docker push -t ${hubUserName}/${imageName}:latest'''
